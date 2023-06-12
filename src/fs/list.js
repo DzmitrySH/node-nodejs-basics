2e9fe2249cb297916538fd74598d7b496de7c868
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-12 13:05:07 +0300

    feat: implement function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import { access, constants, readdir} from "node:fs/promises";
+const fsFailed = "FS operation failed";
+const inFolder = "src/fs/files/";
+
 const list = async () => {
-    // Write your code here 
+    let flagDir
+    try {
+        try {
+            await access(inFolder, constants.F_OK);
+            flagDir = true;
+        } catch {
+            flagDir = false;
+        }
+        if(!flagDir)
+          throw new Error(fsFailed);
+
+        const files = await readdir(inFolder);
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 await list();
