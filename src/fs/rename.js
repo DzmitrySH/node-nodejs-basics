5cde1bcc5f87088baa4dafd7c75e311f78e4bf1c
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-12 12:21:10 +0300

    feat: implement function rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { access, constants, rename as renameFile } from "node:fs/promises";
+const fsFailed = "FS operation failed";
+const fileTxt = "src/fs/files/wrongFilename.txt";
+const fileMd = "src/fs/files/properFilename.md";
+
 const rename = async () => {
-    // Write your code here 
+    let flagsFileMd;
+    try {
+        try {
+            await access(fileMd, constants.F_OK);
+            flagsFileMd = true;
+        } catch (error) {
+            flagsFileMd = false;
+        }
+        if(flagsFileMd)
+          throw new Error(fsFailed);
+
+          await renameFile(fileTxt, fileMd);
+    } catch (error) {
+        console.log(error);
+        if (error.code === "ENOENT") {
+          throw new Error(fsFailed);
+        }
+    }
 };
 
 await rename();
