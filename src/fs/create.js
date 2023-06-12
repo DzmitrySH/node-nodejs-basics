113040206718979093b3973f2713d602c38fa356
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-12 11:04:20 +0300

    feat: implement fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'node:fs/promises';
+
+const pathDir =  "src/fs/files/fresh.txt";
+const fileContent = "I am fresh and young";
+const fsFailed = "FS operation failed";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(pathDir, fileContent, { flag: "wx" });
+  } catch (error) {
+    console.error(error);
+    if (error.code === "EEXIST") {
+        throw new Error(fsFailed);
+      }
+  }
 };
 
 await create();
