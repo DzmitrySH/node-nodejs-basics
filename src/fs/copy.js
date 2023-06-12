aefa84a3444e415090fb3db967a0cd288cdfc7c6
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-12 11:55:53 +0300

    feat: implement function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import { cp, access, constants } from "node:fs/promises";
+
+const originalFolder = "src/fs/files";
+const copyFolder = "src/fs/files_copy";
+const fsFailed = "FS operation failed";
+
 const copy = async () => {
-    // Write your code here 
+    let flagsFolderCopy; 
+    try {
+        try {
+            await access(copyFolder, constants.F_OK);
+            flagsFolderCopy = true;
+        } catch (error) {
+            flagsFolderCopy = false;
+        }
+        if(flagsFolderCopy)
+          throw new Error(fsFailed);
+          
+        await cp(originalFolder, copyFolder, { recursive: true });
+    } catch (error) {
+        console.log(error);
+        if (error.code === "ENOENT") {
+          throw new Error(fsFailed);
+        }
+    } 
 };
 
 await copy();
