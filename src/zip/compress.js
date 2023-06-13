5af05c771de2ca2e484342406095f4c79b667ce1
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-13 15:42:20 +0300

    feat: function decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -3,18 +3,18 @@ import { createGzip } from "node:zlib";
 import { createReadStream, createWriteStream } from "node:fs";
 
 const fileOriginal = "src/zip/files/fileToCompress.txt";
-const fileToGzip ="src/zip/files/archive.gz";
+const fileToGzip = "src/zip/files/archive.gz";
 
 const compress = async () => {
-    try {
-        const readStreamOriginal = createReadStream(fileOriginal);
-        const writeStreamGzip = createWriteStream(fileToGzip);
-        const gZip = createGzip();
+  try {
+    const readStreamOriginal = createReadStream(fileOriginal);
+    const writeStreamGzip = createWriteStream(fileToGzip);
+    const gZip = createGzip();
 
-        await pipeline(readStreamOriginal, gZip, writeStreamGzip);
-    } catch (error) {
-       console.log(error); 
-    }
+    await pipeline(readStreamOriginal, gZip, writeStreamGzip);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { pipeline } from "stream/promises";
+import { createGunzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+
+const originalGzip = "src/zip/files/archive.gz";
+const fileDecompress = "src/zip/files/fileToCompress.txt";
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const readStreamGzip = createReadStream(originalGzip);
+    const writeStreamOriginal = createWriteStream(fileDecompress);
+    const gunZip = createGunzip();
+
+    await pipeline(readStreamGzip, gunZip, writeStreamOriginal);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await decompress();
