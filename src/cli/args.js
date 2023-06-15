cac5bdad8119cb51e9c5ccd05ef145c27dc4c8e5
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-15 22:44:51 +0300

    feat: implement function args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,8 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arsResult = process.argv.reduce((acc,value, index, array) =>
+    value.startsWith("--") ? [...acc, `${value.replace("--", "")} is ${array[index + 1]}`] : acc, []).join(", ");
+
+  console.log(arsResult);
 };
 
 parseArgs();
