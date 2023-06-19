050024a77bfe39cbfa0edaa4c205415a0e7d0180
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-19 08:18:29 +0300

    feat: implement function main

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,8 +3,8 @@ import { parentPort, workerData } from 'node:worker_threads';
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    const { value } = workerData;
-    parentPort.postMessage(nthFibonacci(value)); 
+    const { num } = workerData;
+    parentPort.postMessage(nthFibonacci(num));
 };
 
 sendResult();
